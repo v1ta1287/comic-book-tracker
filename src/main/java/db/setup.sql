e7ff452275cb5cee46e099787f388883894c67ee
Vita1287 <jzho0079@student.monash.edu>
2024-04-05 12:43:57 +1100

    SQL setup

---
+++ src/main/java/db/setup.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE comicDB;
+
+USE comicDB;
+
+CREATE TABLE Users (
+Userid int NOT NULL AUTO_INCREMENT,
+Username varchar(255) NOT NULL,
+Password varchar(255) NOT NULL,
+PRIMARY KEY (Userid)
+);
+
+CREATE TABLE Comics (
+Comicid int NOT NULL AUTO_INCREMENT,
+Name varchar(255) NOT NULL,
+Description varchar(255),
+Rating int,
+Category varchar(255),
+Recommended boolean,
+Userid int,
+PRIMARY KEY (Comicid),
+FOREIGN KEY (Userid) REFERENCES Users(Userid)
+);
+
+CREATE TABLE Characters (
+Characterid int NOT NULL AUTO_INCREMENT,
+Name varchar(255) NOT NULL,
+Description varchar(255),
+age int,
+status varchar(255),
+Comicid int,
+PRIMARY KEY (Characterid),
+FOREIGN KEY (Comicid) REFERENCES Comics(Comicid)
+);
