48d7ebbeee189b88e19a56c940da7905300e7309
Andy Zhong <andyzhong8@gmail.com>
2024-04-05 13:41:29 +1100

    UNIQUE attributes

--- src/main/java/db/setup.sql
+++ src/main/java/db/setup.sql
@@ -4,14 +4,14 @@ USE comicDB;
 
 CREATE TABLE Users (
 Userid int NOT NULL AUTO_INCREMENT,
-Username varchar(255) NOT NULL,
+Username varchar(255) UNIQUE NOT NULL,
 Password varchar(255) NOT NULL,
 PRIMARY KEY (Userid)
 );
 
 CREATE TABLE Comics (
 Comicid int NOT NULL AUTO_INCREMENT,
-Name varchar(255) NOT NULL,
+Name varchar(255) UNIQUE NOT NULL,
 Description varchar(255),
 Rating int,
 Category varchar(255),
@@ -23,7 +23,7 @@ FOREIGN KEY (Userid) REFERENCES Users(Userid)
 
 CREATE TABLE Characters (
 Characterid int NOT NULL AUTO_INCREMENT,
-Name varchar(255) NOT NULL,
+Name varchar(255) UNIQUE NOT NULL,
 Description varchar(255),
 age int,
 status varchar(255),
